d7d8f1c84095ab7aceeec0744937ee2f435d2a2b
jowartt <medinajowartt@gmail.com>
2022-07-20 22:35:47 -0400

    repuestasjs1

---
+++ Archivo-ejemplo.js
@@ -0,0 +1,27 @@
+/*variables
+una variable es un recipiente donde podemos guardar valores para utilizarlos despues.
+existen 3 tipos de variables  var, let, const.
+
+strings
+los strings son lineas de cualquier tipo de texto que esta dentro de " "
+' ' simples o dobles.
+
+funciones
+las funciones nos sirven para no esvribir un codigo doble de lo mismo a la hora de programar.
+en lugar de eso usamos una funcion previamente establecida.
+
+funciones argumentos
+los argumentos son como varibles que estasn dentro de la funcion 
+
+Funcion return 
+es la unica manera en la cual un argumento puede escapar de una funcion 
+
+Declaracion if 
+if es como un si. como si le dijeras a la conputadora si (if) es mayor de edad dejalo entrar 
+normalmente de acompaña de un (else) que seria y si no (else) no estra
+
+valores booleanos 
+los valores booleanos son true y false ques en españos seria verdadero o falso esto puede pareser facil al 
+principio pero se conplicara con el tiempo
+
+*/
